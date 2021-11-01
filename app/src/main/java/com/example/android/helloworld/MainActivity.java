51fc5c1bab4d011e8b47a10ecfd45635ef872025
Achaka <oen-feedback@yandex.ru>
2021-11-02 01:41:49 +0300

    1.1 codelab done

--- app/src/main/java/com/example/android/helloworld/MainActivity.java
+++ app/src/main/java/com/example/android/helloworld/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.android.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    private static final String LOG_TAG_EXCEPTION = "Exception";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(LOG_TAG, "Happy Birthday to Cthulhu");
+        try {
+            exceptionGenerator();
+        } catch (RuntimeException e) {
+            Log.e(LOG_TAG_EXCEPTION, e.getMessage());
+        }
+    }
+
+    public void exceptionGenerator() throws RuntimeException {
+        throw new RuntimeException("Generated Exception");
     }
 }
